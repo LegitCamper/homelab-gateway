f03d4a650a4cb1a509c0dc27126c14d00675a397
sawyer bristol <sawyerbristol@gmail.com>
2023-08-21 11:39:57 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-apt-get update && apt-get upgrade && apt-get install -y ansible python3 python3-venv docker.io helix && systemctl enable docker
+apt-get update
+apt-get upgrade 
+apt-get install -y ansible ansible-galaxy python3 python3-venv docker.io helix 
+systemctl enable docker
 
-wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/requirements.yml && ansible-galaxy install -r requirements.yml
+wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/requirements.yml 
+ansible-galaxy install -r requirements.yml
 ansible-pull -U https://github.com/LegitCamper/homelab-proxy.git
 
-tailscale login && tailscale up && systemctl enable tailscale
+tailscale login
+systemctl enable tailscale
+tailscale up
