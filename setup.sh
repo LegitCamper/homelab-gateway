cc3b532c988347b5c8fae3ffecd34fd398d53267
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-29 20:42:57 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 apt-get update
 apt-get upgrade -y 
-apt-get install -y ansible python3 python3-venv docker.io
-systemctl enable docker
-
-wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/requirements.yml 
-ansible-galaxy install -r requirements.yml
-ansible-pull -U https://github.com/LegitCamper/homelab-proxy.git
-
+curl -fsSL https://tailscale.com/install.sh | sh
 tailscale login
 systemctl enable tailscale
 tailscale up
+
+iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j DNAT --to-destination homelab
+iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j DNAT --to-destination homelab
+iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $(hostname -I) -d homelab
+iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $(hostname -I) -d homelab
