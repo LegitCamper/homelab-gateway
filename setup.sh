5dd2e98670b2a10f634f541dfd61d1604093fecf
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-29 22:33:34 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ tailscale login
 systemctl enable tailscale
 tailscale up
 
+MYIP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6 | tail -n 1)
+
 iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
 iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
-iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $(hostname -I) -d $(tailscale ip -4 homelab)
-iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $(hostname -I) -d $(tailscale ip -4 homelab)
+iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab)
+iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $MYIP -I) -d $(tailscale ip -4 homelab)
