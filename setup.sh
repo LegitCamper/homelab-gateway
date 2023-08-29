085eab1fc236064622823a655d0a0ed1e7d67818
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-29 22:53:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ tailscale up
 
 MYIP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6 | tail -n 1)
 
-iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
-iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
-iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab)
-iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab)
+iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab):8080
+iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab):8080
+iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab):8080
+iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab):8080
