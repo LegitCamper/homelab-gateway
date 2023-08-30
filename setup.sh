5d5e342fe79337afe99dad6e16f8e50575c7275a
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-30 11:40:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ tailscale login
 tailscale up
 
 MYIP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6 | head -n 1)
-HLIP=$(tailscale ip -4 homelab)
+HLIP=$(tailscale ip -4 homelab):8080
 MYTSIP=$(tailscale ip -4)
 EXT=eth0
 INT=tailscale0
@@ -15,7 +15,7 @@ echo 1 >/proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
 # basic firewall to force auth for domain
-iptables -A INPUT -i $EXT -p tcp -s $MYIP -P 22 -j REJECT --reject-with tcp-reset 
+iptables -A INPUT -i $EXT -p tcp --dport 22 -j REJECT --reject-with tcp-reset
 
 # forward traffic to homelab
 iptables -A PREROUTING -i EXT --table nat -p tcp --dport all -j DNAT --to-destination $HLIP
