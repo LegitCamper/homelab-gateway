e76d2d3d91ed7914c062f6faf870f8b33c3d551f
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-30 11:44:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,16 @@ HLIP=$(tailscale ip -4 homelab):8080
 MYTSIP=$(tailscale ip -4)
 EXT=eth0
 INT=tailscale0
+PORTS=1:65535
 
 echo 1 >/proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
-# basic firewall to force auth for domain
-iptables -A INPUT -i $EXT -p tcp --dport 22 -j REJECT --reject-with tcp-reset
-
 # forward traffic to homelab
-iptables -A PREROUTING -i EXT --table nat -p tcp --dport all -j DNAT --to-destination $HLIP
-iptables -A POSTROUTING -o INT --table nat -p tcp --dport all -j SNAT --to-source $MYTSIP
-iptables -A PREROUTING -i EXT --table nat -p udp --dport all -j DNAT --to-destination $HLIP
-iptables -A POSTROUTING -o INT --table nat -p udp --dport all -j SNAT --to-source $MYTSIP
+iptables -A PREROUTING -i EXT --table nat -p tcp --dport $PORTS -j DNAT --to-destination $HLIP
+iptables -A POSTROUTING -o INT --table nat -p tcp --dport $PORTS -j SNAT --to-source $MYTSIP
+iptables -A PREROUTING -i EXT --table nat -p udp --dport $PORTS -j DNAT --to-destination $HLIP
+iptables -A POSTROUTING -o INT --table nat -p udp --dport $PORTS -j SNAT --to-source $MYTSIP
 
 # make rules persistent
 sudo iptables-save > /etc/iptables/rules.v4
