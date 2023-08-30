537aebc0b4d8bf62a4e2c08036afd4a330d3df2c
Sawyer Bristol <sawyerbristol@gmail.com>
2023-08-30 10:12:25 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,23 @@
 apt-get update
 apt-get upgrade -y 
+apt-get install iptables-persistent
 curl -fsSL https://tailscale.com/install.sh | sh
 tailscale login
 systemctl enable tailscale
 tailscale up
 
 MYIP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6 | head -n 1)
-TAILSCALEIP=$(tailscale ip -4 homelab)
+HLIP=$(tailscale ip -4 homelab)
+MYTSIP=$(tailscale ip -4)
+EXT=eth0
+INT=tailscale0
 
 echo 1 >/proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
-#iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
-#iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j DNAT --to-destination $(tailscale ip -4 homelab)
-#iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab)
-#iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j SNAT --to-source $MYIP -d $(tailscale ip -4 homelab)
+iptables --append PREROUTING -i EXT --table nat --protocol tcp --dport 1:65535 -j DNAT --to-destination $HLIP
+iptables --append POSTROUTING -o INT --table nat --protocol tcp --dport 1:65535 -j SNAT --to-source $MYTSIP
+iptables --append PREROUTING -i EXT --table nat --protocol udp --dport 1:65535 -j DNAT --to-destination $HLIP
+iptables --append POSTROUTING -o INT --table nat --protocol udp --dport 1:65535 -j SNAT --to-source $MYTSIP
 
-#iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 1:65535 -j DNAT --to $(tailscale ip -4 homelab)
-#iptables -A FORWARD -p tcp -d $(tailscale ip -4 homelab) --dport 1:65535 -j ACCEPT
-#iptables -A PREROUTING -t nat -i eth0 -p udp --dport 1:65535 -j DNAT --to $(tailscale ip -4 homelab)
-#iptables -A FORWARD -p udp -d $(tailscale ip -4 homelab) --dport 1:65535 -j ACCEPT
-
-#iptables -A FORWARD -i eth0 -o tailscale0 -j ACCEPT
-#iptables -A FORWARD -i eth0 -o tailscale0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-#iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-
-
-iptables --in-interface eth0 --append PREROUTING --table nat --protocol tcp --destination $MYIP --dport 1:65535 --jump DNAT --to-destination $TAILSCALEIP
-iptables --out-interface tailscale0 --append POSTROUTING --table nat --protocol tcp --destination $TAILSCALEIP --dport 1:65535 --jump SNAT --to-source $MYIP
-sudo iptables -t nat -A POSTROUTING -p tcp --dport 1:65535 -j MASQUERADE
-iptables --in-interface eth0 --append PREROUTING --table nat --protocol udp --destination $MYIP --dport 1:65535 --jump DNAT --to-destination $TAILSCALEIP
-iptables --out-interface tailscale0 --append POSTROUTING --table nat --protocol udp --destination $TAILSCALEIP  --dport 1:65535 --jump SNAT --to-source $MYIP
-sudo iptables -t nat -A POSTROUTING -p udp --dport 1:65535 -j MASQUERADE
+sudo iptables-save > /etc/iptables/rules.v4
