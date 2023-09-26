524d152c6c21de228cc3731ccf66ada6a4254710
Sawyer Bristol <sawyerbristol@gmail.com>
2023-09-26 15:30:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,20 +6,21 @@ tailscale login
 tailscale up
 
 MYIP=$(ip addr show eth0 | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6 | head -n 1)
-HLIP=$(tailscale ip -4 homelab):80
+HLIP=$(tailscale ip -4 homelab)
 MYTSIP=$(tailscale ip -4)
 EXT=eth0
 INT=tailscale0
-PORTS=1:65535
+PORTSC=1:65535
+PORTSH=1-65535
 
 echo 1 >/proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
 # forward traffic to homelab
-iptables -t nat -A PREROUTING -i EXT -p tcp --dport $PORTS -j DNAT --to-destination $HLIP
-iptables -t nat -A POSTROUTING -o INT -p tcp --sport $PORTS -j SNAT --to-source $MYTSIP:$PORTS
-iptables -t nat -A PREROUTING -i EXT -p udp --dport $PORTS -j DNAT --to-destination $HLIP
-iptables -t nat -A POSTROUTING -o INT -p udp --sport $PORTS -j SNAT --to-source $MYTSIP:$PORTS
+iptables -t nat -m multiport -A PREROUTING -i EXT -p tcp --dport $PORTSC -j DNAT --to-destination $HLIP
+iptables -t nat -m multiport -A POSTROUTING -o INT -p tcp --sport $PORTSC -j SNAT --to-source $MYTSIP:$PORTSH
+iptables -t nat -m multiport -A PREROUTING -i EXT -p udp --dport $PORTSC -j DNAT --to-destination $HLIP
+iptables -t nat -m multiport -A POSTROUTING -o INT -p udp --sport $PORTSC -j SNAT --to-source $MYTSIP:$PORTSH
 
 # make rules persistent
 sudo iptables-save > /etc/iptables/rules.v4
