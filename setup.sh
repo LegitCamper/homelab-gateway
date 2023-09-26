55f600e63c696e829f8b3b5f7a494330292306ba
sawyer bristol <sawyerbristol@gmail.com>
2023-09-26 21:17:59 +0000

    traefik binary

--- setup.sh
+++ setup.sh
@@ -7,5 +7,10 @@ tailscale up --advertise-exit-node --hostname=homelab-proxy
 
 wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/traefik.toml
 wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/dynamic.toml
+mkdir /etc/traefik
+cp traefik.toml /etc/traefik/traefik.toml
+cp dynamic.toml /etc/traefik/dynamic.toml
 
-docker run -d --net=host -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v $PWD/dynamic.toml:/etc/traefik/dynamic.toml traefik
+wget https://github.com/traefik/traefik/releases/download/v2.9.6/traefik_v2.9.6_linux_amd64.tar.gz
+tar xzvf traefik_v2.9.6_linux_amd64.tar.gz
+./traefik
