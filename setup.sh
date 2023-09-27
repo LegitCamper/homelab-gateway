e2b81a47ef92453d73e2149e159f2a1c3ee5250d
sawyer bristol <sawyerbristol@gmail.com>
2023-09-27 12:53:05 -0600

    fix setup

--- setup.sh
+++ setup.sh
@@ -5,11 +5,9 @@ curl -fsSL https://tailscale.com/install.sh | sh
 tailscale login
 tailscale up --advertise-exit-node --hostname=homelab-proxy
 
-wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/traefik.toml
-wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/dynamic.toml
 mkdir /etc/traefik
-cp traefik.toml /etc/traefik/traefik.toml
-cp dynamic.toml /etc/traefik/dynamic.toml
+wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/traefik.toml -O /etc/traefik/traefik.toml
+wget https://raw.githubusercontent.com/LegitCamper/homelab-proxy/main/traefik/dynamic.toml -O /etc/traefik/dynamic.toml
 
 wget https://github.com/traefik/traefik/releases/download/v2.9.6/traefik_v2.9.6_linux_amd64.tar.gz
 tar xzvf traefik_v2.9.6_linux_amd64.tar.gz
